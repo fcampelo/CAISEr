77c19bb3703eb17ba00923f47c0cc9fa2b70c8b4
Felipe Campelo <fcampelo@gmail.com>
2019-07-03 18:42:14 +0100

    Update package startup message

--- R/zzz.R
+++ R/zzz.R
@@ -5,5 +5,5 @@ Check https://github.com/fcampelo/CAISEr/tree/v0.3.3 for
 previous versions (e.g., to reproduce the results of
 F. Campelo, F. Takahashi, 'Sample size estimation for power
 and accuracy in the experimental comparison of algorithms',
-Journal of Heuristics 25(2):305-338, 2019.")
+Journal of Heuristics 25(2):305-338, 2019).")
 }
