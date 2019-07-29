71792407c9a38bc02ae4b92205f1dc41ad18220b
Felipe Campelo <fcampelo@gmail.com>
2019-07-29 15:30:36 +0100

    Update onAttach.R

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,5 +1,5 @@
 .onAttach <- function(...) {
-  packageStartupMessage("\nAttaching package CAISEr 1.14
+  packageStartupMessage("\nAttaching package CAISEr 1.0.14
 This version is not compatible with versions 0.XX
 Check https://github.com/fcampelo/CAISEr/tree/v0.3.3 for previous version")
 }
